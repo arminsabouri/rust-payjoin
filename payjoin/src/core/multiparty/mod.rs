35f876e8a92078c307a0cc210b4daa974369e82c
Armin Sabouri <me@arminsabouri.com>
2026-06-02 12:15:41 -0400

    Multiparty BIP321 uri
    Supporting pj param used in v2 and an optional mppj
    query param indicating this is an intent for multiparty 
    session. Importantly this bitcoin uri does not include 
    a bitcoin address neccecitating the use of bip321.

--- payjoin/src/core/multiparty/mod.rs
+++ payjoin/src/core/multiparty/mod.rs
@@ -1,2 +1,5 @@
 #[cfg_attr(docsrs, doc(cfg(feature = "multiparty")))]
 pub mod linked_mailbox;
+#[cfg_attr(docsrs, doc(cfg(feature = "multiparty")))]
+pub mod uri;
+pub use uri::{build_multiparty_pj_uri, MultipartyPjUri, MultipartyUriError};
